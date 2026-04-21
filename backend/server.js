6c92964e79a9ca8c416b0b5897a11e127598e62e
Krissh K. <krissh.k2005@gmail.com>
2026-04-21 14:46:28 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname,'uploads')));
+app.use('/api/members', require('./routes/members'));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> app.listen(5000, ()=> console.log('Server running on 5000')))
+.catch(console.error);
